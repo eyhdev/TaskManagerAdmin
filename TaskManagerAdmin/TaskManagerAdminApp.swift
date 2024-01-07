454f794760830d460cb0fdc67f136aea4099de5c
eyhdev <alaqsa335@gmail.com>
2024-01-07 17:05:15 +0300

    Initial Commit

---
+++ TaskManagerAdmin/TaskManagerAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAdminApp.swift
+//  TaskManagerAdmin
+//
+//  Created by eyh.mac on 7.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
