dccc7ab33139978f0ec206c87decb62111267fe3
eyhdev <alaqsa335@gmail.com>
2024-01-08 08:50:28 +0300

    Admin codes added.

---
+++ TaskManagerAdmin/Views/UsersList.swift
@@ -0,0 +1,18 @@
+//
+//  UsersList.swift
+//  TaskManagerAdmin
+//
+//  Created by eyh.mac on 7.01.2024.
+//
+
+import SwiftUI
+
+struct UsersList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsersList()
+}
